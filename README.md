e8aebfcdff22eaff56b315b61acd962157dd2886
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-02 23:46:03 +0900

    Init postgres db

---
+++ README.md
@@ -0,0 +1,6 @@
+# GongZuo Infra
+
+### 開発環境でのDBの接続
+```bash
+psql -U root -h 127.0.0.1 -p 11468 -d gongzuo
+```
---
+++ scripts/0_init.sql
@@ -0,0 +1,25 @@
+set client_encoding = 'UTF8';
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS contents (
+    content_id SERIAL PRIMARY KEY,
+    -- 0: 仕事, 1: 仕事以外
+    content_kind INTEGER NOT NULL CHECK (content_kind IN (0, 1)),
+    content VARCHAR(1023) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS gongzuo (
+    id SERIAL PRIMARY KEY,
+    user_id SERIAL NOT NULL REFERENCES users(id),
+    content_id SERIAL NOT NULL REFERENCES contents(content_id),
+    started_at TIMESTAMP NOT NULL,
+     -- NULL: not ended yet
+    ended_at TIMESTAMP
+);
