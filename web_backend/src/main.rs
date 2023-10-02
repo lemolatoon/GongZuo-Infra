daf84ac359ab267583f503c0117b6ac9bed76c1d
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-03 00:47:22 +0900

    Init sqlx

--- web_backend/src/main.rs
+++ web_backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
+use dotenvy;
+use once_cell::sync::Lazy;
+use sqlx::postgres::PgPoolOptions;
+
+static DATABASE_URL: Lazy<String> = Lazy::new(|| {
+    dotenvy::dotenv().unwrap();
+
+    std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set. Maybe you forgot to run `cargo make dotenv`")
+});
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&DATABASE_URL)
+        .await
+        .unwrap();
+
+    let users = sqlx::query!("SELECT * FROM users")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    println!("users: {:?}", &users);
+    if let Some(user) = users.first() {
+        println!("id: {}, username: {}", user.id, user.username);
+    }
 }
