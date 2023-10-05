8e41696f060bbca7b58af89f5d1667e34e54cc8c
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-05 23:38:16 +0900

    Add CORS layer

--- web_backend/src/router/root.rs
+++ web_backend/src/router/root.rs
@@ -1,11 +1,24 @@
 use axum::{
+    http::{header::CONTENT_TYPE, HeaderValue, Method},
     routing::{get, post},
     Router,
 };
+use tower_http::cors::CorsLayer;
 
 use crate::{db, handlers, router};
 
 pub fn app_router(db: db::DB) -> Router {
+    let allowed_orgins: Vec<HeaderValue> = ["http://localhost:3000"]
+        .into_iter()
+        .map(|o| o.parse().unwrap())
+        .collect();
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_origin(allowed_orgins)
+        .allow_credentials(true)
+        .allow_headers([CONTENT_TYPE]);
+
     Router::new()
         .route("/", get(|| async { "Hello, world! from '/'" }))
         .route("/users", get(handlers::users::users))
@@ -15,4 +28,5 @@ pub fn app_router(db: db::DB) -> Router {
         .route("/me", get(handlers::users::me))
         .nest("/gongzuo", router::gongzuo::gongzuo_router())
         .with_state(db)
+        .layer(cors)
 }
