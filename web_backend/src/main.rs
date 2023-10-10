970ab29d319e7e1b0d12b62bd0fa356cbba6d99c
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-10 13:40:14 +0900

    Add tls support

--- web_backend/src/main.rs
+++ web_backend/src/main.rs
@@ -6,9 +6,13 @@ pub mod router;
 pub mod session;
 pub mod util;
 
-use std::net::SocketAddr;
+use std::{
+    net::{IpAddr, SocketAddr},
+    path::PathBuf,
+};
 
 use axum::{self};
+use axum_server::tls_rustls::RustlsConfig;
 use db::user::UserHandlerTrait;
 use once_cell::sync::Lazy;
 use sqlx::postgres::PgPoolOptions;
@@ -48,8 +52,41 @@ async fn main() {
     let port = std::env::var("PORT")
         .map_or(None, |p| p.parse().ok())
         .unwrap_or(3001);
+    let host: IpAddr = std::env::var("HOST")
+        .map(|i| i.parse().unwrap())
+        .unwrap_or([127, 0, 0, 1].into());
+
+    let addr = SocketAddr::from((host, port));
+
+    let tls = std::env::var("TLS").ok().is_some();
+
+    if tls {
+        let config = RustlsConfig::from_pem_file(
+            std::env::var("CERT_PATH")
+                .map(|path| PathBuf::from(path))
+                .unwrap_or(
+                    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                        .join("cert")
+                        .join("cert.pem"),
+                ),
+            std::env::var("PRIVATE_KEY_PATH")
+                .map(|path| PathBuf::from(path))
+                .unwrap_or(
+                    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                        .join("cert")
+                        .join("privkey.pem"),
+                ),
+        )
+        .await
+        .unwrap();
+        axum_server::bind_rustls(addr, config)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+        return;
+    }
+
     // run it with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
     println!("Listening on {}", &addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
